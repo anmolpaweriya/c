f98f891c5a615a7f9d8168d543fad9a46b22f76e
Anisole <64343922+anisolepro@users.noreply.github.com>
2022-12-24 23:01:26 +0530

    Stack using array

--- stackArray.c
+++ stackArray.c
@@ -1,1 +1,59 @@
+#include <stdio.h>
 
+void push(int arr[], int value, int len)
+{
+    arr[++len] = value;
+}
+
+int pop(int arr[], int len)
+{
+    return arr[len];
+}
+int main()
+{
+    int stack[69], len = -1, value, choice;
+start:
+    printf("\n\n\nChoose an operation ----------->");
+    printf("\n\t1. push Element");
+    printf("\n\t2. pop Element");
+    printf("\n\t3. Exit");
+    printf("\n\nEnter Your Choice :-) ");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+        // push
+        if (len < 70)
+        {
+
+            printf("\n\nEnter value to Push :-) ");
+            scanf("%d", &value);
+            push(stack, value, len++);
+        }
+        else
+        {
+            printf("\n\n---------[ Stack OverFlow ]-------------------");
+        }
+        break;
+
+    case 2:
+        // pop
+        if (len >= 0)
+        {
+            printf("\n\nPoped value is %d", pop(stack, len--));
+        }
+        else
+        {
+            printf("\n\n---------[ Stack UnderFlow ]-------------------");
+        }
+        break;
+
+    case 3:
+        goto end;
+        break;
+    }
+    goto start;
+end:
+    return 0;
+}
