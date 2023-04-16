d0e02c32b2eaa6ae792443b5f61ebeb4803c9fe8
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:10:23 +0530

    check Odd or Even in C

--- BSc2ndYear/oddEven.c
+++ BSc2ndYear/oddEven.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
 
+main(){
+int numb;
+clrscr();
+printf("Enter a Number : ");
+scanf("%d",&numb);
+
+numb%2 == 0 ? printf("Even Number"):printf("Odd Number");
+
+getch();
+}
