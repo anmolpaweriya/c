28ea399d68871149d00fe16c180189e901f4becb
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:02:24 +0530

    Area of Circle in c

--- BSc2ndYear/AreaCircle.c
+++ BSc2ndYear/AreaCircle.c
@@ -1,1 +1,13 @@
 
+#include<stdio.h>
+#include<conio.h>
+
+main(){
+float radius;
+clrscr();
+printf("Enter Radius : ");
+scanf("%f",&radius);
+printf("Area of Circle = %.2f",3.142*radius*radius);
+
+getch();
+}
