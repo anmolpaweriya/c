9652556c5a8810d352ced995bcdd20859477de8c
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:05:14 +0530

    Fibonacce series in c

--- BSc2ndYear/Fibonacce.c
+++ BSc2ndYear/Fibonacce.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
 
+int fibo(int l){
+
+if(l == 1) return 0;
+if(l == 2) return 1;
+return fibo(l-1)+fibo(l-2);
+}
+
+main(){
+int i = 1,len;
+clrscr();
+printf("Enter length of series : ");
+scanf("%d",&len);
+printf("\nFibbonacci series --------->");
+for(;i<=len;i++){
+printf("\n%d",fibo(i));
+}
+getch();
+}
