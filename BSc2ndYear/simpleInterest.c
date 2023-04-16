16dece045a9ffc687ac0288641384562e533a21a
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:12:51 +0530

    Find Simple Interest In C

--- BSc2ndYear/simpleInterest.c
+++ BSc2ndYear/simpleInterest.c
@@ -1,1 +1,32 @@
+#include<iostream.h>
+#include<conio.h>
 
+class className{
+long principal,rate,time;
+
+public :
+className(int principal,int rate,int time){
+this->principal = principal;
+this->rate = rate;
+this->time = time;
+
+}
+
+void getValue(){
+cout<<"Principal = "<<principal<<endl;
+cout<<"Rate = "<<rate<<endl;
+cout<<"time = "<<time<<endl;
+cout<<"Amount = "<<principal*rate*time/100<<endl;
+}
+
+};
+
+main(){
+clrscr();
+
+className obj(2500,10,2);
+obj.getValue();
+
+
+ getch();
+}
