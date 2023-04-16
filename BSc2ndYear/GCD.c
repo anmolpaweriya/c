1f65bae5c3664b2e3cdbd1c46c38bce5f40b80e2
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:05:58 +0530

    Find GCD in C

--- BSc2ndYear/GCD.c
+++ BSc2ndYear/GCD.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
 
+main(){
+
+int a,b,i,gcd=1;
+clrscr();
+printf("Enter 2 Numbers : ");
+scanf("%d%d",&a,&b);
+for(i = 1;i<=a&&i<=b;i++){
+if(a%i == 0 && b%i == 0){
+gcd = i;
+}
+}
+printf("\nGCD = %d", gcd);
+
+getch();
+}
