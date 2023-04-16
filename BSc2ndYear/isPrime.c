d70c6b84d4e13dcf8afa6452b8b349c5ac0500a6
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-04-16 10:09:15 +0530

    Check Prime Number in C

--- BSc2ndYear/isPrime.c
+++ BSc2ndYear/isPrime.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
 
+main(){
+float a,b,c,max;
+clrscr();
+printf("Enter 3 numbers : ");
+scanf("%f%f%f",&a,&b,&c);
+max = a>b ? a :b;
+max = c>max ? c:max;
+printf("\nGreatest Number = %f",max);
+getch();
+}
