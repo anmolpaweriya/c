b937d3c0689b62560b984ae3767ef33b82f30400
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-05-01 20:54:47 +0530

    Command Line Arguments in C

--- commandLineArgument.c
+++ commandLineArgument.c
@@ -1,1 +1,15 @@
+// command Line Arguments in C
+#include <stdio.h>
 
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    printf("%d\n", argc);
+    for (int i = 1; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+
+    printf("Anisole \n");
+    return 0;
+}
