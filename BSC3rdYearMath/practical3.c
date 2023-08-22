86c22d3dc3fabe29cf6a5987e5b7026918f2de99
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-08-22 22:22:40 +0530

    Lagrange's interpolation folmula

--- BSC3rdYearMath/practical3.c
+++ BSC3rdYearMath/practical3.c
@@ -1,1 +1,75 @@
+#include <stdio.h>
+#define max 69
 
+/*
+
+
+
+
+
+
+WAP to demonstrate Lagrange's interpolation folmula
+
+
+
+
+
+*/
+
+int main()
+{
+
+    float arrx[max], arry[max], num, den, x, y = 0;
+    int i = 0, j, n;
+
+    /*
+
+
+
+
+          taking all inputs
+
+
+      */
+    printf("Enter Length of List : ");
+    scanf("%d", &n);
+
+    printf("Enter values of x and y : \n");
+
+    for (; i < n; i++)
+        scanf("%f%f", &arrx[i], &arry[i]);
+
+    printf("Enter value of x to get value of y : ");
+    scanf("%f", &x);
+
+    /*
+
+
+
+
+
+
+
+    apply formula
+
+    */
+    for (i = 0; i < n; i++)
+    {
+        num = den = 1;
+
+        for (j = 0; j < n; j++)
+        {
+            if (i == j)
+                continue;
+
+            num *= x - arrx[j];
+            den *= arrx[i] - arrx[j];
+        }
+
+        y += (num / den) * arry[i];
+    }
+
+    printf("\n\n Answer : %f", y);
+
+    return 0;
+}
